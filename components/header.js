2fbf71bd3b84fc1ba0fc80b469cc242db8d61080
Kristopher <kristopher@ust.hk>
2019-03-28 13:44:15 +0800

    Update header.js

--- components/header.js
+++ components/header.js
@@ -39,7 +39,7 @@ const Header = () => (
         </h1>
         <div className="ruler"></div>
           {/*<a data-toggle="modal" data-target="#messageModal" className="text-white btn btn-outline-light btn-lg bold-outline-btn">Join a party</a>*/}
-           <a href="https://ust.az1.qualtrics.com/jfe/form/SV_e5RUnMPs5KKWgcd" target="_blank" className="text-white btn btn-outline-light btn-lg bold-outline-btn">Join a party</a>
+           <a href="https://ust.az1.qualtrics.com/jfe/form/SV_8wZUIGZILTyY9Hn" target="_blank" className="text-white btn btn-outline-light btn-lg bold-outline-btn">Join a party</a>
       </div>
       <style jsx>{`
         .bold-outline-btn {
