78e2e2c01f74edb39c2f88bd0e6f9a300f3f9c2f
Kristopher Lam <kris@sandb0x.io>
2019-04-07 19:04:06 +0800

    minor routing fixes

--- components/nav.js
+++ components/nav.js
@@ -43,14 +43,12 @@ export default class Nav extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div className="container">
-          <Link href="/">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">
-              <div style={{display: 'flex', alignItems: 'center'}}>
-                <img src="/static/assets/images/logo.png" style={{height: '36px'}}/>
-                <img src="/static/assets/images/brand.png" style={{height: '96px'}}/>
-              </div>
-            </a>
-          </Link>
+          <a className="navbar-brand js-scroll-trigger" href="/">
+            <div style={{display: 'flex', alignItems: 'center'}}>
+              <img src="/static/assets/images/logo.png" style={{height: '36px'}}/>
+              <img src="/static/assets/images/brand.png" style={{height: '96px'}}/>
+            </div>
+          </a>
           <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <i className="fas fa-bars"></i>
           </button>
