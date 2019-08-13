b0f7a7a99860b9b36c75efb2c34e9559d1252ca5
FGAlbero <44393729+FGAlbero@users.noreply.github.com>
2019-08-13 16:58:15 +0800

    Update terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -21,7 +21,7 @@ const TermsAndCondition = () => (
             Remake will attempt to service your product as described and for the charges shown on the Work Receipt plus any applicable tax. Remake may restrict service to one (1) product per customer during your visit to a repair party session. The service offered by Remake is not covered by any warranty.
           </p>
           <p className="lead description">Remake understands that your data may be valuable to you. <strong>Data loss during service is always a possibility</strong>, and in some cases, data may be unrecoverable, erased, or reformatted during service. For this reason, it is your sole responsibility to back up all existing data, software, and/or programs from your product, and to decide whether to erase any such data from your product, prior to receiving service. Remake is not responsible for loss, recovery, or compromise of data, software or programs, or loss of use of your product or other equipment arising out of the services provided by Remake. You represent that your product does not contain any illegal files or data.</p>
-          <p className="lead description">For this reason, it's recommended that you <strong>back up your device and wipe it, prior to submission for service</strong>. Remake technicians and team abide by strict confidentiality rules, and will try to preserve your privacy in all instances, yet this does not guarantee against a potential data leakage, for which Remake is not responsible.</p>
+          <p className="lead description">For this reason, it's recommended that you <strong>back up your device and wipe it prior to submission for service</strong>. Remake technicians and team abide by strict confidentiality rules, and will try to preserve your privacy in all instances, yet this does not guarantee against a potential data leakage, for which Remake is not responsible.</p>
         </div>
   
         
@@ -30,7 +30,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            <strong>Remake reserves the right to return the product to you without servicing it</strong>. Remake services have not been officially authorised by any hardware manufacturer. Consequently, Remake will not be responsible for any damage to the product that occurs during the repair process. If damage results, Remake will seek your authorization for any additional costs for completing service even if the product is covered by warranty. In most scenarios, however, Remake’s service will equateto voiding your warranty contract with the device’s manufacturer. If you decline authorization, Remake may return your product unrepaired in the damaged condition without any responsibility.
+            <strong>Remake reserves the right to return the product to you without servicing it</strong>. Remake services have not been officially authorised by any hardware manufacturer. Consequently, Remake will not be responsible for any damage to the product that occurs during the repair process. If damage results, Remake will seek your authorization for any additional costs for completing service even if the product is covered by warranty. In most scenarios, however, Remake’s service will equate to voiding your warranty contract with the device’s manufacturer. If you decline authorization, Remake may return your product unrepaired in the damaged condition without any responsibility.
           </p>
         </div>
         
@@ -39,7 +39,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-          As part of service, Remake may install system software updates that will prevent your Remake product from reverting to an earlier version of the system software. Third party applications installed on your Remake product may not be compatible or work with your Remake product as a result of the system software update.
+          As part of service, Remake may install system software updates that will prevent your product from reverting to an earlier version of the system software. Third party applications installed on your product may not be compatible or work with your product as a result of the system software update.
           </p>
         </div>
         
@@ -48,7 +48,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            If service requires labor and/or parts not specified on the previous screen, <strong>Remake may seek your approval of a revised estimate</strong>. If you do not agree that Remake may revise the charges, Remake may return your product unrepaired.
+            If service requires parts not specified on the diagnosing session, <strong>Remake may seek your approval of a revised estimate</strong>. If you do not agree that Remake may revise the charges, Remake may return your product unrepaired.
           </p>
         </div>
 
@@ -66,7 +66,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-          Remake cannot warrant (1) that service will be performed in a competent and workmanlike manner, but it warrants (2) that all parts used to service your product will be free from defects in materials and workmanship for any period of fourteen (14) days, unless otherwise specified by Remake. The foregoing warranty is an express limited warranty and in the event of breach, Remake will either (i) re-perform the service, (ii) repair or replace the part. THIS WARRANTY AND ASSOCIATED REMEDIES ARE EXCLUSIVE AND IN LIEU OF ALL OTHER WARRANTIES, REMEDIES, ANDCONDITIONS, WHETHER ORAL OR WRITTEN, EXPRESS OR IMPLIED. REMAKESPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES INCLUDING, WITHOUTLIMITATION, WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IF REMAKE CANNOT LAWFULLY DISCLAIM IMPLIED WARRANTIES, THEN ALL SUCH WARRANTIES ARE LIMITED IN DURATION TO THE EXPRESS LIMITED WARRANTY.SOME STATES (COUNTRIES AND PROVINCES) DO NOT ALLOW LIMITATIONS ON HOW LONG AN IMPLIED WARRANTY (OR CONDITION) MAY LAST, SO THE LIMITATION DESCRIBED ABOVE MAY NOT APPLY TO YOU.
+          Remake cannot warrant (1) that service will be performed in a competent and workmanlike manner, but it warrants (2) that all parts used to service your product will be free from defects in materials and workmanship for any period of fourteen (14) days, unless otherwise specified by Remake. The foregoing warranty is an express limited warranty and in the event of breach, Remake will either (i) re-perform the service, (ii) repair or replace the part. THIS WARRANTY AND ASSOCIATED REMEDIES ARE EXCLUSIVE AND IN LIEU OF ALL OTHER WARRANTIES, REMEDIES, AND CONDITIONS, WHETHER ORAL OR WRITTEN, EXPRESS OR IMPLIED. REMAKE SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES INCLUDING, WITHOUT LIMITATION, WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IF REMAKE CANNOT LAWFULLY DISCLAIM IMPLIED WARRANTIES, THEN ALL SUCH WARRANTIES ARE LIMITED IN DURATION TO THE EXPRESS LIMITED WARRANTY. SOME STATES (COUNTRIES AND PROVINCES) DO NOT ALLOW LIMITATIONS ON HOW LONG AN IMPLIED WARRANTY (OR CONDITION) MAY LAST, SO THE LIMITATION DESCRIBED ABOVE MAY NOT APPLY TO YOU.
           </p>
         </div>
 
@@ -75,8 +75,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            TO THE MAXIMUM EXTENT PERMITTED BY LAW, REMAKE AND ITS AFFILIATES, WILLUNDER NO CIRCUMSTANCES BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL ORCONSEQUENTIAL DAMAGES RESULTING FROM SERVICES PROVIDED OR UNDER ANYOTHER LEGAL THEORY, INCLUDING BUT NOT LIMITED TO LOSS OF REVENUE; LOSS OFACTUAL OR ANTICIPATED PROFITS (INCLUDING LOSS OF PROFITS ON CONTRACTS);LOSS OF THE USE OF MONEY; LOSS OF ANTICIPATED SAVINGS; LOSS OF BUSINESS;LOSS OF OPPORTUNITY; LOSS OF GOODWILL; LOSS OF REPUTATION; LOSS OF,DAMAGE TO, OR CORRUPTION OF DATA; OR ANY COSTS OF RECOVERING,PROGRAMMING, OR RESTORING ANY PROGRAM OR DATA STORED OR USED WITHYOUR PRODUCT AND ANY FAILURE TO MAINTAIN THE CONFIDENTIALITY OF DATASTORED ON YOUR PRODUCT. THE FOREGOING LIMITATION SHALL NOT APPLY TODEATH OR PERSONAL INJURY CLAIMS. REMAKE SPECIFICALLY DOES NOT WARRANT THAT IT WILL BE ABLE TO (i) REPAIR OR REPLACE YOUR PRODUCT WITHOUT RISK TO OR LOSS OF PROGRAMS OR DATA, AND (ii) MAINTAIN THE CONFIDENTIALITY OF DATA.IF ANY PRODUCT SHOULD BE DAMAGED OR LOST WHILE IN REMAKE’S CUSTODY,REMAKE’S LIABILITY WILL BE LIMITED TO THE COST OF REPAIR OR REPLACEMENT OF
-            THE AFFECTED PRODUCT. OTHERWISE, REMAKE’S LIABILITY FOR ANY AND ALL DAMAGE SHALL IN NO EVENT EXCEED THE PAYMENTS RECEIVED BY REMAKE FORSERVICES PROVIDED PURSUANT TO THESE TERMS. THE REMEDIES SET FORTH HEREINSHALL BE YOUR SOLE AND EXCLUSIVE REMEDIES FOR ANY BREACH BY REMAKE UNDERTHESE TERMS AND CONDITIONS. SOME STATES (COUNTRIES AND PROVINCES) DO NOT ALLOW THE EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE LIMITATION OR EXCLUSION MAY NOT APPLY TO YOU.
+            TO THE MAXIMUM EXTENT PERMITTED BY LAW, REMAKE AND ITS AFFILIATES, WILL UNDER NO CIRCUMSTANCES BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR CONSEQUENTIAL DAMAGES RESULTING FROM SERVICES PROVIDED OR UNDER ANY OTHER LEGAL THEORY, INCLUDING BUT NOT LIMITED TO LOSS OF REVENUE; LOSS OF ACTUAL OR ANTICIPATED PROFITS (INCLUDING LOSS OF PROFITS ON CONTRACTS); LOSS OF THE USE OF MONEY; LOSS OF ANTICIPATED SAVINGS; LOSS OF BUSINESS; LOSS OF OPPORTUNITY; LOSS OF GOODWILL; LOSS OF REPUTATION; LOSS OF, DAMAGE TO, OR CORRUPTION OF DATA; OR ANY COSTS OF RECOVERING, PROGRAMMING, OR RESTORING ANY PROGRAM OR DATA STORED OR USED WITH YOUR PRODUCT AND ANY FAILURE TO MAINTAIN THE CONFIDENTIALITY OF DATA STORED ON YOUR PRODUCT. THE FOREGOING LIMITATION SHALL NOT APPLY TO DEATH OR PERSONAL INJURY CLAIMS. REMAKE SPECIFICALLY DOES NOT WARRANT THAT IT WILL BE ABLE TO (i) REPAIR OR REPLACE YOUR PRODUCT WITHOUT RISK TO OR LOSS OF PROGRAMS OR DATA, AND (ii) MAINTAIN THE CONFIDENTIALITY OF DATA. IF ANY PRODUCT SHOULD BE DAMAGED OR LOST WHILE IN REMAKE’S CUSTODY, REMAKE’S LIABILITY WILL BE LIMITED TO THE COST OF REPAIR OR REPLACEMENT OF THE AFFECTED PRODUCT. OTHERWISE, REMAKE’S LIABILITY FOR ANY AND ALL DAMAGE SHALL IN NO EVENT EXCEED THE PAYMENTS RECEIVED BY REMAKE FOR SERVICES PROVIDED PURSUANT TO THESE TERMS. THE REMEDIES SET FORTH HEREIN SHALL BE YOUR SOLE AND EXCLUSIVE REMEDIES FOR ANY BREACH BY REMAKE UNDER THESE TERMS AND CONDITIONS. SOME STATES (COUNTRIES AND PROVINCES) DO NOT ALLOW THE EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE LIMITATION OR EXCLUSION MAY NOT APPLY TO YOU.
           </p>
         </div>
         
@@ -85,7 +84,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            In the event that Remake temporarily keeps your product for servicing, should you have not claimed your product and paid all charges due within sixty (60) days after being notified by Remake that your product has been serviced and ready for collection, Remake will consider your product abandoned by you and you give Remake the right to dispose of your abandoned product by whatever method Remake sees fit in accordance with applicable law. Proceeds derived from any sale or disposal of your abandoned product may be applied by Remake towards payment of any moneys owing by you to Remake.
+            In the event that Remake temporarily keeps your product for servicing, should you have not claimed your product and/or replacement parts and paid all charges due within sixty (60) days after being notified by Remake that your product and/or replacement parts have been serviced and ready for collection, Remake will consider your product and/or replacement parts abandoned by you and you give Remake the right to dispose of them by whatever method Remake sees fit in accordance with applicable law. Proceeds derived from any sale or disposal of your abandoned product may be applied by Remake towards the payment of any moneys owing by you to Remake.
           </p>
         </div>
         
@@ -94,7 +93,7 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            If service involves transferring information or installing software, you represent thatyou have the legal right to copy the information and agree to the terms of the software license, and you authorize Remake to transfer the information and accept such terms on your behalf in performing the service.
+            If service involves transferring information or installing software, you represent that you have the legal right to copy the information and agree to the terms of the software license, and you authorize Remake to transfer the information and accept such terms on your behalf in performing the service.
           </p>
         </div>
 
@@ -103,20 +102,29 @@ const TermsAndCondition = () => (
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            Remake’s rates for service are determined internally on a case-by-case basis, but are often calculated as an approximation of the cost of the replaced part plus any applicable shipping. When possible, and at Remake’s discretion, <strong>Remake will attempt to reimburse you part of the net cost of the replaced part(s),</strong>if any, not including shipping. The final amount for reimbursement, if any, is determined by Remake and is final. Should you have not claimed your reimbursement due within fourteen (14) days after being notified by Remake that you are eligible for reimbursement, Remake will consider your reimbursement amount donated by you to Remake and you give Remake the right to use it in whatever way it sees fit.
+           Remake’s rates for service are determined internally on a case-by-case basis, but are often calculated as an approximation of the cost of the replaced part plus any applicable shipping. 
           </p>
         </div>
 
-        <div className="col-sm-1">
+     <div className="col-sm-1">
           <span className="numeric-bulletin">11.</span>
         </div>
         <div className="col-sm-11">
           <p className="lead description">
-            These T&Cs are governed by the laws of Hong Kong (without giving effect to its conflict of law provisions).
+           According to the existing referral program, and at Remake’s discretion, <strong>Remake will attempt to reimburse you part of the net cost of the replaced part(s)</strong>, if any, not including shipping. The final amount for reimbursement, if any, is determined by Remake and is final. Should you have not claimed your reimbursement due within fourteen (14) days after being notified by Remake that you are eligible for reimbursement, Remake will consider your reimbursement amount donated by you to Remake and you give Remake the right to use it in whatever way it sees fit.
+          </p>
+        </div>
+
+        <div className="col-sm-1">
+          <span className="numeric-bulletin">12.</span>
+        </div>
+        <div className="col-sm-11">
+          <p className="lead description">
+            These T&Cs are governed by the laws of Hong Kong SAR China (without giving effect to its conflict of law provisions).
           </p>
         </div>
         <div className="col-sm-1">
-          <span className="numeric-bulletin">12.</span>
+          <span className="numeric-bulletin">13.</span>
         </div>
         <div className="col-sm-11">
           <p className="lead description">
@@ -124,7 +132,7 @@ const TermsAndCondition = () => (
           </p>
         </div>
         <div className="col-sm-1">
-          <span className="numeric-bulletin">13.</span>
+          <span className="numeric-bulletin">14.</span>
         </div>
         <div className="col-sm-11">
           <p className="lead description">
