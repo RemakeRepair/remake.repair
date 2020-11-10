ff08d8a8a4b74f493abee4f3e1b1e0e3e3ba5d97
Kristopher <kristopher@ust.hk>
2020-11-10 23:41:55 +0800

    Update team.js

--- components/team.js
+++ components/team.js
@@ -4,15 +4,15 @@ const team = [
   { 
     name: 'Fernando García Albero',
     bio: [
-      '<strong> Founder </strong>',
-      'Now a public policy master's student, I have been fixing stuff since I was a teenager.',
+      `Founder`,
+      `Now a public policy master's student, I have been fixing stuff since I was a teenager.`,
       'Strongly passionate about society, environment and technology and finding ways to produce impactful change in the world other than by going vegan.'
     ],
     avatar: '/static/assets/images/fernando.jpg' },
   { 
     name: 'Nupur Dhawan',
     bio: [
-      '<strong> UCL Branch Coordinator </strong>',
+      'UCL Branch Coordinator',
       'With ample work experience in consultancy, I have now deciced to learn more about NGO work in sustainability.',
       'I am thrilled to see how much this community has achieved to date, and I am excited to be part of this journey',
       'Find me around UCL anytime!'
@@ -21,7 +21,7 @@ const team = [
   { 
     name: 'Johnson Chong',
     bio: [
-      '<strong> HKUST Branch Coordinator </strong>',
+      'HKUST Branch Coordinator',
       'Year 3 Computer Engineering local student and baseball addict.',
       'I am enthusiastic about anything that makes the Earth a better place for everyone.',
       'Go ahead and send me anything fixable other than phones, anything meaning anything. I’ll gladly fix it for you, if I can…'
@@ -53,10 +53,10 @@ const Members = () => (
           <div className="card py-4 h-100">
             <div className="card-body text-center">
               <img src={member.avatar} height="180" alt={member.name} className="rounded-circle"/>
-              <div className="small text-black-50 mt-3">{member.name}</div>
+              <div className="small text-black-50 mt-3"{member.name}</div>
               {
                 member.bio.map((paragraph, j) => (
-                  <p key={j}>{paragraph}</p>
+                  <p className={`${j===0?'font-weight-bold':''}`} key={j}>{paragraph}</p>
                 ))
               }
             </div>
