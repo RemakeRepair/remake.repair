4701e052f70a5d62d1730401096f124c6516b33b
FGAlbero <44393729+FGAlbero@users.noreply.github.com>
2020-11-10 11:46:46 +0000

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,7 @@ const Layout = (props) => (
     {props.children}
     <footer className="bg-black small text-center text-white-50">
       <div className="container">
-        <i className="fas fa-home"></i> Based in Hong Kong; Homegrown in <i className="fas fa-university"></i> HKUST
+        <i className="fas fa-home"></i> Based in universities; Homegrown in <i className="fas fa-university"></i> Hong Kong
         <div className="row justify-content-center">
           <Link href="/privacy"><a>Privacy Policy</a></Link>
           <span style={{margin: '0 20px'}}>|</span>
