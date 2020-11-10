aefbc999f0cbc522e0ff2a2dea60f1110ded4926
FGAlbero <44393729+FGAlbero@users.noreply.github.com>
2020-11-10 11:54:57 +0000

    Update team.js

--- components/team.js
+++ components/team.js
@@ -17,7 +17,7 @@ const team = [
       'I am thrilled to see how much this community has achieved to date, and I am excited to be part of this journey',
       'Find me around UCL anytime!'
     ],
-    avatar: '/static/assets/images/Alvin.jpg' },
+    avatar: '/static/assets/images/nupur.jpg' },
   { 
     name: 'Johnson Chong',
     bio: [
