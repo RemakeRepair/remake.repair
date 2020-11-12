0a9a3ca7ee63a496ee8c395caa913af84234d964
FGAlbero <44393729+FGAlbero@users.noreply.github.com>
2020-11-12 14:22:21 +0000

    Update recruitment.js

--- pages/recruitment.js
+++ pages/recruitment.js
@@ -7,16 +7,16 @@ const Recruitment = () => (
     <div className="jumbotron jumbotron-fluid" style={{paddingTop: '200px'}}>
       <div className="container">
         <h1 className="display-4" style={{fontFamily: `'Cereal', sans-serif`, fontWeight: 700}}>Recruitment</h1>
-        <p className="lead">Want to join REMAKE as a technician? Want to learn all there is to know about repair? Want to be able to manage the first free repair community in Asia? Or the first one in UCL? You’re in the right place.</p>
+        <p className="lead">Want to join REMAKE as a technician? Want to learn all there is to know about repair? Want to be able to manage the first free repair community in a university? You’re in the right place.</p>
         <hr className="my-4"/>
-        <p className="lead description">Before we get started, make sure you know exactly what we do, and why we do it. If you’re still unsure, you might want to head to the “About Us” page and come back here a little later.</p>
+        <p className="lead description">Before we get started, make sure you know exactly what we do, and why we do it. If you’re still unsure, talk to us at join@remake.repair</p>
       </div>
     </div>
 
     <div className="container">
       <div className="row">
-        <h4 style={{fontFamily: `'Cereal', sans-serif`, fontWeight: 700}}>As a technician, however, there are a number of things you should also know: </h4>
-        <p className="lead description">We have two technician categories: Coordinator and Assistant Technician (AT). You join as an AT and, if you show strong dedication towards the project, you’ll be upped to the status of Coordinator in no time. The greater the dedication, the more perks you get (like a fancy Pro Tech Toolkit all for yourself!)</p>
+        <h4 style={{fontFamily: `'Cereal', sans-serif`, fontWeight: 700}}>As a technician, there are a number of things you should know: </h4>
+        <p className="lead description">We have two technician categories: Coordinator and Assistant Technician (AT). You join as an AT and, as soon as you acquire all the skills and knolwede you need, you’ll be upped to the status of Coordinator in no time. The greater the dedication, the more perks you get (like a fancy Pro Tech Toolkit all for yourself!)</p>
         <p className="lead description">As an AT, you are subject to the following responsibilities, duties, and benefits:</p>
       </div>
       <div className="row">
@@ -32,7 +32,7 @@ const Recruitment = () => (
             <tr>
               <td>
                 <p className="description">
-                  Good teamwork, collaborative learning and skill-sharing
+                  Good teamwork, collaborative learning and skill-sharing spirit
                 </p>
               </td>
               <td>
@@ -49,17 +49,17 @@ const Recruitment = () => (
             <tr>
               <td>
                 <p className="description">
-                  Willingness to pick up the necessary repairing skills  
+                  Willingness to pick up the necessary repair skills rapidly
                 </p>
               </td>
               <td>
                 <p className="description">
-                  Diagnose issues and service devices during some Repair Parties, depending on availability
+                  Operate as a technician, diagnosing issues and servicing devices, during most Repair Parties
                 </p>
               </td>
               <td>
                 <p className="description">
-                  Training on how to repair most handheld electronic devices for free
+                  Access to REMAKE's diagnosing and repair tools
                 </p>
               </td>
             </tr>
@@ -71,15 +71,19 @@ const Recruitment = () => (
               </td>
               <td>
                 <p className="description">
-                Contribute ideas to expand REMAKE’s community and improve the project
+                Contribute to other activities and projects as possible to help expand REMAKE's community and values
                 </p>
               </td>
-              <td></td>
+              <td>
+                 <p className="description">
+                Personalised training on how to repair most consumer electronic devices for free
+                </p> 
+              </td>
             </tr>
           </tbody>
         </table>
         <div className="offset-md-1 col-md-11">
-          <p className="lead description">&#9654; As such, you’ll be in a position to contribute as a technician during our Repair Parties. There are, usually, six parties in a semester; they take place once every two weeks (usually on Sundays), and each of them takes about 5 to 6 hours. You can attend these parties depending on your own availability, but remember, the more you do, the more likely you get upped to the status of Coordinator. If you refuse to attend any, you will be removed from the project. </p>
+          <p className="lead description">&#9654; As such, you’ll be in a position to contribute as a technician during our Repair Parties. There are, usually, four to six parties in a semester/term; they take place once every two weeks (usually on Sundays), and each of them takes about 5 to 6 hours. You can attend these parties depending on your own availability, but remember, the more you do, the more likely you get upped to the status of Coordinator. Lack of enthusiasm will be frowned upon! </p>
         </div>
         <div className="offset-md-1 col-md-11">
           <p className="lead description">&#9654; You <b>do not</b> need any prior experience in repair in order to apply. We will teach you everything you need to know. </p>
@@ -89,7 +93,7 @@ const Recruitment = () => (
         </div>
       </div>
       <div className="row">
-        <h4 style={{fontFamily: `'Cereal', sans-serif`, fontWeight: 700}}>Still want to apply? Let’s get to it! </h4>
+        <h4 style={{fontFamily: `'Cereal', sans-serif`, fontWeight: 700}}>Let’s get to it! </h4>
         <div style={{display: 'flex', width: '100%'}}>
           <iframe frameBorder="0" border="0" style={{borderStyle: 'none',width: '100%',height: '640px'}} src="https://remake.wispform.com/81dcf6a7"></iframe>
         </div>
